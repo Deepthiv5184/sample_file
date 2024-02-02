bce27b4014135a3a41103a1cd2ec2c14269867b4
Deepthiv5184 <158201985+Deepthiv5184@users.noreply.github.com>
2024-02-02 10:58:58 -0500

    Add files via upload

---
+++ sampletest/sampletest/sampletestApp.swift
@@ -0,0 +1,17 @@
+//
+//  sampletestApp.swift
+//  sampletest
+//
+//  Created by DEEPTHI on 2/2/24.
+//
+
+import SwiftUI
+
+@main
+struct sampletestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
